699a6be5467596105db119316e85d2181040d887
ivanreyess <melvre14@gmail.com>
2023-05-22 14:55:07 -0600

    feat: adding more test

--- src/test/java/com/sv/simpleapi/controller/EmployeeControllerTest.java
+++ src/test/java/com/sv/simpleapi/controller/EmployeeControllerTest.java
@@ -1,7 +1,10 @@
 package com.sv.simpleapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sv.simpleapi.DTO.EmployeeDTO;
+import com.sv.simpleapi.model.Department;
 import com.sv.simpleapi.model.Employee;
+import com.sv.simpleapi.response.EmployeeResponse;
 import com.sv.simpleapi.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,16 +15,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -58,11 +65,46 @@ class EmployeeControllerTest {
     }
 
     @Test
-    void getAllEmployees() {
+    void getAllEmployees() throws Exception {
+        Employee employee1 = Employee.builder()
+                .id(2L)
+                .name("Karen")
+                .lastName("Recinos")
+                .email("karen@email.com")
+                .createdAt(localDateTime)
+                .build();
+
+        EmployeeResponse employeeResponse = EmployeeResponse
+                .builder()
+                .employees(Stream.of(employee, employee1).map(this::toEmployeeDTO).toList())
+                .last(true)
+                .pageNo(0)
+                .pageSize(10)
+                .totalPages(1)
+                .totalElements(2)
+                .build();
+
+        given(employeeService.findAll(0, 10, "id", "asc")).willReturn(employeeResponse);
+
+        ResultActions perform = mockMvc.perform(get("/api/v1/employees"));
+
+        perform.andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.employees[0].id", is(1)))
+        .andExpect(jsonPath("$.employees[1].id", is(2)))
+        .andExpect(jsonPath("$.employees[1].name", is("Karen")));
+
     }
 
     @Test
-    void getEmployee() {
+    void getEmployee() throws Exception {
+
+        long employeeId = 1L;
+        given(employeeService.findOne(employeeId)).willReturn(Optional.of(employee));
+
+        mockMvc.perform(get("/api/v1/employees/{id}", employeeId))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -103,10 +145,62 @@ class EmployeeControllerTest {
     }
 
     @Test
-    void updateEmployee() {
+    @DisplayName("PUT /api/v1/employee/{id} success ")
+    void updateEmployee() throws Exception {
+
+        employee.setDepartment(Department.DATA_ENGINEER);
+
+        given(employeeService.save(employee)).willReturn(employee);
+        given(employeeService.exist(employee.getId())).willReturn(true);
+
+
+        mockMvc.perform(put("/api/v1/employees/{id}", employee.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee))
+                ).andExpect(status().isOk());
+
+    }
+
+    @Test
+    @DisplayName("PUT /api/v1/employee/{id} bad request ")
+    void updateEmployeeFailed() throws Exception {
+
+        employee.setDepartment(Department.DATA_ENGINEER);
+        employee.setId(null);
+
+        given(employeeService.save(employee)).willReturn(employee);
+        given(employeeService.exist(employee.getId())).willReturn(true);
+
+
+        mockMvc.perform(put("/api/v1/employees/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee))
+        ).andExpect(status().isBadRequest());
+
     }
 
     @Test
     void partialUpdateEmployee() {
     }
+
+    Employee toEmployee(EmployeeDTO employeeDTO) {
+        return Employee.builder()
+                .name(employeeDTO.name())
+                .lastName(employeeDTO.lastName())
+                .id(employeeDTO.id())
+                .email(employeeDTO.email())
+                .build();
+
+    }
+
+    EmployeeDTO toEmployeeDTO(Employee employee) {
+        return EmployeeDTO.builder()
+                .name(employee.getName())
+                .lastName(employee.getLastName())
+                .id(employee.getId())
+                .email(employee.getEmail())
+                .build();
+
+    }
+
 }
